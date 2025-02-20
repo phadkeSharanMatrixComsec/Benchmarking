5d92057d670a82ae3acb55845b1cb41858e783ae
sharan.phadke <sharan.phadke@matrixcomsec.com>
2025-02-20 10:52:44 +0530

    fix: sourceOnDemand

--- scripts/create-mtx-live-stream-config.py
+++ scripts/create-mtx-live-stream-config.py
@@ -6,8 +6,10 @@ def generate_yaml(file_name, n, base_url):
     for i in range(n):
         stream_name = f"mystream{i}"
         source_url = f"{base_url}/{stream_name}"
+        sourceOnDemand = f"yes";
         data["paths"][stream_name] = {
-            "source": source_url
+            "source": source_url,
+            "sourceOnDemand": True
         }
     
     with open(file_name, "w") as file:
@@ -16,4 +18,4 @@ def generate_yaml(file_name, n, base_url):
     print(f"YAML file '{file_name}' generated successfully!")
 
 # Example usage
-generate_yaml("mediamtx_live_stream_100.yml", 100, "rtsp://192.168.27.79:8554")
+generate_yaml("mediamtx_live_stream_200.yml", 200, "rtsp://192.168.27.79:8554")
